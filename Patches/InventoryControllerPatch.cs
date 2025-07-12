f7a7e2639515c4a5f6c39a590f2655fcf0cbf2b9
Trench-foot <jamesodell66@gmail.com>
2025-07-11 20:49:58 -0500

    Initial Commit

---
+++ Patches/InventoryControllerPatch.cs
@@ -0,0 +1,45 @@
+﻿using EFT.InventoryLogic;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+
+namespace ScavsWithArmbands.Patches
+{
+    public class InventoryControllerPatch : ModulePatch
+    {
+        private static GClass3180 baseType;
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(InventoryController), 
+                    nameof(InventoryController.ReplaceInventory));
+        }
+        [PatchPrefix]
+        public static bool Prefix(InventoryController __instance, Inventory newInventory)
+        {
+            baseType = __instance as GClass3180;
+
+            if (__instance.Inventory != null)
+            {
+                baseType.RemoveItemEvent -= new Action<GEventArgs3>(__instance.Inventory.UpdateTotalWeight);
+                baseType.AddItemEvent -= new Action<GEventArgs2>(__instance.Inventory.UpdateTotalWeight);
+                baseType.RefreshItemEvent -= new Action<GEventArgs18>(__instance.Inventory.UpdateTotalWeight);
+                __instance.Inventory = null;
+            }
+            __instance.Inventory = newInventory;
+            // Skip original logic removing armband slots from scavs
+            //if (__instance.Profile.Side == EPlayerSide.Savage)
+            //{
+            //    __instance.Inventory.Equipment.GetSlot(EquipmentSlot.ArmBand).Deleted = true;
+            //}
+            baseType.RemoveItemEvent += new Action<GEventArgs3>(__instance.Inventory.UpdateTotalWeight);
+            baseType.AddItemEvent += new Action<GEventArgs2>(__instance.Inventory.UpdateTotalWeight);
+            baseType.RefreshItemEvent += new Action<GEventArgs18>(__instance.Inventory.UpdateTotalWeight);
+            if (__instance.Inventory.Stash != null && __instance.Inventory.Stash.CurrentAddress == null)
+            {
+                __instance.Inventory.Stash.CurrentAddress = baseType.CreateItemAddress();
+            }
+            return false; // Skip the original method execution
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using Comfort.Common;
+using EFT.InventoryLogic;
+using EFT.UI;
+using EFT.UI.Screens;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ScavsWithArmbands.Patches;
+
+namespace ScavsWithArmbands
+{
+    [BepInPlugin("ScavsWithArmbands", "ScavsWithArmbands", "1.0.0")]
+    [BepInDependency("com.SPT.core", "3.11.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new InventoryControllerPatch().Enable();
+        }
+    }
+}
